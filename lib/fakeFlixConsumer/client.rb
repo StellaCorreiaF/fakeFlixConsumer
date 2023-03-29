14ecf8835c69cc5a2bc9eba740f9da33cb84eafb
StellaCorreiaF <stellafernanda@icloud.com>
2023-03-28 21:21:23 -0300

    corrige bloco

--- lib/fakeFlixConsumer/client.rb
+++ lib/fakeFlixConsumer/client.rb
@@ -17,7 +17,7 @@ module FakeFlixConsumer
 
     def list_movies_by_genre(genre_id)
       list =  request_movies "/movies?genre_id=#{genre_id}"
-      list.map { |m| Movie.new(title: movie['title'], genre_id: movie['genre_id']) }
+      list.map { |movie| Movie.new(title: movie['title'], genre_id: movie['genre_id']) }
 
     end
 
